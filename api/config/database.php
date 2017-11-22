eb72d4ab8ce7f7ea05e19ba5d12d3ac3fe3071de
XITEHIP <504738414@qq.com>
2017-11-22 21:39:43 +0800

    debug

---
+++ api/config/database.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'master' => [
+        'user' => 'root',
+        'password' => 'xxxxxx',
+        'dbms' => 'mysql',
+        'host' => 'localhost',
+        'dbname' => 'dzztest'
+    ],
+    'slave' => [
+        ['weigth' => 3, 'user' => 'root', 'password' => 'hangcom', 'dbms' => 'mysql', 'host' => 'localhost', 'dbname' => 'dzztest'],
+        ['weigth' => 7, 'user' => 'root', 'password' => 'hangcom', 'dbms' => 'mysql', 'host' => 'localhost', 'dbname' => 'dzztest'],
+        ['weigth' => 1, 'user' => 'root', 'password' => 'hangcom', 'dbms' => 'mysql', 'host' => 'localhost', 'dbname' => 'dzztest']
+    ]
+];
